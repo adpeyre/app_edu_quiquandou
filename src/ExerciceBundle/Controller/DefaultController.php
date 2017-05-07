b7a1915bb504df4675930a6d900ac83029bc0a5c
Adrien <adrien.peyre@live.fr>
2017-05-07 11:47:33 +0200

    bower pour gérer les dépendances front-end

--- src/ExerciceBundle/Controller/DefaultController.php
+++ src/ExerciceBundle/Controller/DefaultController.php
@@ -5,6 +5,11 @@ namespace ExerciceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+
+/**
+* @Route("/student")
+*/
+
 class DefaultController extends Controller
 {
     /**
